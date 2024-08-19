0adc6a5165fb2b65e0208424b3f7f647b83411f4
Vitor Scarabelli <scarabelli.vitinho@gmail.com>
2024-08-19 11:49:16 -0300

    Commit dos exercicios

---
+++ Hello world java/lista1/src/AloMundo.java
@@ -0,0 +1,10 @@
+package lista1;
+
+public class AloMundo {
+	public static void main(String args[]) {
+		//escreva ("Olá Mundo")
+		System.out.println("Hello World");
+		System.out.println("Olá mundo");
+	}
+	
+}
